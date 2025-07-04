f7d7c0569c65a8ac1a84f5db6b84b6e7b24695f0
vvsglock <vvsglock@yandex.ru>
2025-07-04 18:21:18 +0300

    ООП

--- main.py
+++ main.py
@@ -1,96 +1,113 @@
 from dataclasses import dataclass
 
-
 @dataclass
-class Enterprises:
+class Enterprice:
     name: str
     prod: str
     profit: int
     tax: int
 
 
-def load_data(file_path: str):
-    records = []
-    with open(file_path, 'r', encoding='utf-8') as file:
-        next(file)
-        for line in file:
-            name, prod, profit, tax = line.strip().split("|")
-            records.append(
-                Enterprises(name, prod, int(profit), int(tax))
-            )
+class Economy:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+        self.records: list[Enterprice] = []
+        self.load_data()
 
-    return records
+    def load_data(self) -> None:
+        with open(self.file_path, 'r', encoding='utf-8') as file:
+            next(file)
+            for line in file:
+                name, prod, profit, tax = line.strip().split("|")
+                self.records.append(
+                    Enterprice(name, prod, int(profit), int(tax))
+                )
+
+    def write_data(self):
+        with open(self.file_path, 'w', encoding='utf-8') as file:
+            file.write("Предприятие|Продукция|Прибыль|Налоги\n")
+            for r in self.records:
+                file.write(f"{r.name}|{r.prod}|{r.profit}|{r.tax}\n")
+
+    def add_enterprice(self, name: str, prod: str, profit: int, tax: int):
+        self.records.append(
+            Enterprice(name, prod, int(profit), int(tax))
+        )
+        self.write_data()
+
+    def total_tax(self):
+        total = 0
+        for r in self.records:
+            total += r.tax
+
+        return total
+
+    def clear_profit(self, name: str):
+        clr = 0
+        for r in self.records:
+            if r.name == name:
+                clr = r.profit - r.tax
+
+        return clr
 
 
-def write_data(file_path: str, records: list[Enterprises]):
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.write("Предприятие|Продукция|Прибыль|Налоги\n")
-        for r in records:
-            file.write(f"{r.name}|{r.prod}|{r.profit}|{r.tax}\n")
+class UserInterface:
+    @staticmethod
+    def menu():
+        print("1. Добавить предприятие")
+        print("2. Общая сумма налогов")
+        print("3. Чистая прибыль предприятия")
 
+    @staticmethod
+    def get_choice():
+        return int(input())
 
-def add_enterprise(records: list[Enterprises], name: str, prod: str, profit: int, tax: int):
-    records.append(
-        Enterprises(name, prod, int(profit), int(tax))
-    )
+    @staticmethod
+    def add_enerprice():
+        print("Введите данные предприятия")
+        name = input("Введите имя:")
+        prod = input("Введите тип продукции")
+        profit = int(input("Введите прибыль"))
+        tax = int(input("Введите сумму налога"))
 
+        return name, prod, profit, tax
 
-def total_sum_tax(records: list[Enterprises]):
-    total = 0
-    for r in records:
-        total += r.tax
-
-    return total
-
-
-def clear_profit(records: list[Enterprises], name: str):
-    profit = 0
-    for r in records:
-        if r.name == name:
-            profit = r.profit - r.tax
-
-    return profit
-
-
-def main():
-    path = "C:/Users/User/PycharmProjects/region/.venv/database.txt"
-
-    records = load_data(path)
-
-    print("1. Добавить предприятие")
-    print("2. Узнать общую сумму налогов")
-    print("3. Узнать чистую прибыль предприятия")
-
-    point = int(input())
-
-    if point == 1:
+    @staticmethod
+    def get_enterprice_name():
         print("Введите название предприятия")
         name = input()
 
-        print("Введите продукцию предприятия")
-        prod = input()
+        return name
 
-        print("Введите прибыль предприятия")
-        profit = int(input())
 
-        print("Введите налог предприятия")
-        tax = int(input())
+def main():
+    file_path = "C:/Users/User/PycharmProjects/region_OOP/database.txt"
+    economy = Economy(file_path)
+    ui = UserInterface()
 
-        add_enterprise(records, name, prod, profit, tax)
-        write_data(path, records)
+    ui.menu()
+    choice = ui.get_choice()
 
-    if point == 2:
-        ttl = total_sum_tax(records)
-        print(f"В казну пришло: {ttl}")
+    if choice == 1:
+        data = ui.add_enerprice()
+        economy.add_enterprice(*data)
+        print("Предприятие добавлено")
 
-    if point == 3:
-        print("Введите предприятие")
-        name = input()
+    if choice == 2:
+        ttl = economy.total_tax()
+        print(f"Общая сумма налогов: {ttl}")
 
-        clr = clear_profit(records, name)
+    if choice == 3:
+        name = ui.get_enterprice_name()
+        prft = economy.clear_profit(name)
 
-        print(f"Чистая прибыль {name} = {clr}")
+        print(f"Чистая прибыль {name} = {prft}")
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
